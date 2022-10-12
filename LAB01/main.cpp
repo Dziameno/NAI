15dea464638f541bac371202728aec36d9400487
Tomasz Woźny <tomaszwozny96@gmail.com>
2022-10-12 12:49:06 +0200

    v1 final

--- LAB01/main.cpp
+++ LAB01/main.cpp
@@ -3,28 +3,43 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <utility>
 #include <vector>
-using mojamapa_t = std::map<std::string, std::string>;
-using mojafunkcja_t = std::function<std::string(std::string)>;
-void wypisz(mojamapa_t mapa, mojafunkcja_t fun) {
-    using namespace std;
-    for (auto kv : mapa) {
-        auto [k, v] = kv;
-        cout << "klucz: " << k << "; wartosc " << fun(v) << endl;
-    }
+#include <complex>
+
+using mojamapa_t = std::map<std::string, int>;
+using mojafunkcja_t = std::function<double(mojamapa_t)>;
+
+void wypisz(mojamapa_t mapa, const mojafunkcja_t& func){
+    std:: cout << func(std::move(mapa)) << std::endl;
 }
+
 int main(int argc, char **argv) {
     using namespace std;
-    map<string, string> mapa = {{"imie", "Jan"}};
+    map<string, int> mapa = {
+            {"arg_0", 1}, // example
+            {"arg_1", 2}
+    };
     map<string, mojafunkcja_t> formatery;
-    formatery["r"] = [](string x) { return "== " + x + " =="; };
-    formatery["p"] = [](string x) { return "__" + x + "__"; };
+    formatery["add"] = [](mojamapa_t add) {
+        return add.at("arg_2") + add.at("arg_3");
+    };
+    formatery["sin"] = [](mojamapa_t si) {
+        double result = si.at("arg_2") * (numbers::pi/180);
+        return sin(result) ;
+    };
+    formatery["mod"] = [](mojamapa_t mod) {
+        return mod.at("arg_2") % mod.at("arg_3");
+    };
     try {
         vector<string> argumenty(argv, argv + argc);
+        for( int i=2; i < argumenty.size(); i++){
+            mapa.insert({{"arg_" + to_string(i), stoi(argumenty.at(i))}});
+        }
         auto selected_f = argumenty.at(1);
-        wypisz(mapa, formatery.at(selected_f));
-    } catch (std::out_of_range aor) {
-        cout << "podaj argument. Dostępne to: ";
+        wypisz(mapa, formatery.at( selected_f));
+    } catch (out_of_range out_of_range) {
+        cout << "Jestem programem do liczenia sumy, sinusa i reszty z dzielenia";
         for (auto [k, v] : formatery) cout << " " << k;
         cout << endl;
     }
