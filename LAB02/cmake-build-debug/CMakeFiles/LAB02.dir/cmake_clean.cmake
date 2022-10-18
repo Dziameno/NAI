2bfba6f201b198eb853368f9d59177885e5711cf
Tomasz Woźny <zuo@MacBook-Pro-Tomasz.local>
2022-10-18 16:14:20 +0200

    v1 final

---
+++ LAB02/cmake-build-debug/CMakeFiles/LAB02.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAB02.dir/main.cpp.o"
+  "CMakeFiles/LAB02.dir/main.cpp.o.d"
+  "LAB02"
+  "LAB02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LAB02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- LAB02/main.cpp
+++ LAB02/main.cpp
@@ -1,57 +1,73 @@
 #include <iostream>
-#include <vector>
 #include <functional>
 #include <random>
+
 /**
- * domain - generate domain points. Throws exception when all the points were returned
+ * domain - generate random domain points
  */
-auto brute_force = [](auto f, auto domain) {
+
+auto random_probe = [](auto f, auto domain, int itr) {
     auto current_p = domain();
     auto best_point = current_p;
     try {
-        while (true) {
+
+        for (int i = 0; i < itr; i++) {
             if (f(current_p) < f(best_point)) {
                 best_point = current_p;
             }
             current_p = domain();
         }
+
     } catch (std::exception &e) {
     }
     return best_point;
 };
-using domain_t = std::vector<double>;
+
 std::random_device rd;
 std::mt19937 mt_generator(rd());
-domain_t hill_climbing(const std::function<double(domain_t)> &f, domain_t minimal_d, domain_t maximal_d, int max_iterations) {
-    domain_t current_p(minimal_d.size());
-    for (int i = 0; i < minimal_d.size(); i++) {
-        std::uniform_real_distribution<double> dist(minimal_d[i], maximal_d[i]);
-        current_p[i] = dist(mt_generator);
-    }
-    for (int iteration = 0; iteration < max_iterations; iteration++) {
-        domain_t new_p(minimal_d.size());
-        for (int i = 0; i < minimal_d.size(); i++) {
-            std::uniform_real_distribution<double> dist(-1.0/128.0, 1.0/128.0);
-            new_p[i] = current_p[i] + dist(mt_generator);
-        }
-        if (f(current_p) > f(new_p)) {
-            current_p = new_p;
-        }
-    }
-    return current_p;
-}
+
 int main() {
-    auto sphere_f = [](double x) {return x*x;};
-    double current_sphere_x = -10;
-    auto sphere_generator = [&]() {
-        current_sphere_x+= 1.0/128.0;
-        if (current_sphere_x >= 10) throw std::invalid_argument("finished");
-        return current_sphere_x;
+    using namespace std;
+
+    auto xy_generator = [&]() {
+        std::uniform_real_distribution<> dis(-10, 10);
+        return std::pair<double, double>(dis(mt_generator), dis(mt_generator));
     };
-    auto best_point = brute_force(sphere_f, sphere_generator);
-    std::cout << "best x = " << best_point << std::endl;
-    auto sphere_f_v = [](domain_t x) {return x[0]*x[0];};
-    auto best2 = hill_climbing(sphere_f_v, {-10},{10},10000);
-    std::cout << "best x = " << best2[0] << std::endl;
+
+    auto sphere_f = [](pair<double, double> sphere) {
+        return pow(sphere.first, 2) + pow(sphere.second, 2);
+    };
+
+    auto three_hump_camel_f = [](pair<double, double> three_hump_camel) {
+        return 2*pow(three_hump_camel.first, 2) -
+        1.05*pow(three_hump_camel.first, 4) +
+        pow(three_hump_camel.first,6) / 6 +
+        (three_hump_camel.first * three_hump_camel.second) +
+        pow(three_hump_camel.second,2);
+    };
+
+    auto matyas_f = [](pair<double, double> matyas) {
+        return 0.26 * (pow(matyas.first, 2) +
+        pow(matyas.second, 2)) -
+        0.48 * (matyas.first * matyas.second);
+    };
+
+    auto booth_f = [](pair<double, double> booth){
+        return pow(booth.first+2*booth.second-7,2) +
+        pow(2*booth.first+booth.second-5,2);
+    };
+
+
+    auto best_point = random_probe(sphere_f, xy_generator, 100);
+    cout << "best sphere domain x = " << best_point.first << " y = " << best_point.second << endl;
+
+    best_point = random_probe(three_hump_camel_f, xy_generator, 100);
+    cout << "best three hump camel domain x = " << best_point.first << " y = " << best_point.second << endl;
+
+    best_point = random_probe(matyas_f, xy_generator, 100);
+    cout << "best matyas domain x = " << best_point.first << " y = " << best_point.second << endl;
+
+    best_point = random_probe(booth_f, xy_generator, 100);
+    cout << "best booth domain x = " << best_point.first << " y = " << best_point.second << endl;
     return 0;
 }
