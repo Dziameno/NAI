4dc0229a6e368d8a8cebf1fcd6e0264174015716
Tomasz Woźny <zuo@MacBook-Pro-Tomasz.local>
2022-12-07 16:19:25 +0100

    lab07 in python

---
+++ LAB07py/main.py
@@ -0,0 +1,33 @@
+import time
+import cv2 as cv
+
+video = cv.VideoCapture(1)
+a = 0
+
+while True:
+    a+=1
+    check, frame = video.read()
+    #cv.imshow('Normal Image', frame)
+
+    mirror = cv.flip(frame, 1)
+    # cv.imshow('Mirror Image', mirror)
+
+    if cv.waitKey(1) == 27:
+        break
+
+    gray = cv.cvtColor(mirror, cv.COLOR_BGR2GRAY)
+    ret, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cv.drawContours(gray, contours, -1, (0, 255, 0), 3)
+
+    cv.imshow('Image with contours', gray)
+
+
+    start = time.time()
+    if len(contours) > 0:
+        c = max(contours, key=cv.contourArea)
+        end = time.time()
+        print(end - start)
+
+video.release()
+cv.destroyAllWindows()
